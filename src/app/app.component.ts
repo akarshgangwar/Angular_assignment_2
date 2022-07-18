fb58878f5f0d1423f961eb87e9f4011bcc17e2a7
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-07-18 15:46:16 +0530

    disabling button when input is empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username="Akarsh"
+  username=""
+  indicator=true;
 
-  
+  checktStatusOfInput(event:Event){
+
+    
+    if(((<HTMLInputElement>event.target).value).length>0)
+    {
+      this.indicator=false
+  }
+  else
+  this.indicator=true;
+  }
+
 }
